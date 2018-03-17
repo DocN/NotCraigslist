b09eb3cc13ce702ed771c1e93738bcc4ba72677a
Ryan C <031247ryan@gmail.com>
2018-03-17 10:00:35 -0700

    created site and added bootstrap shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: MainpageComponent,
+  },
+  {
+    path: '',
+    component: MainpageComponent,
+  }
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
